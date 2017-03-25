8456b864881809b115f453ed3fbd25ccb193bead
ManavGupta04 <manavgupta@hotmail.co.uk>
2017-03-25 21:41:07 +0000

    made something map thing

--- app/src/main/java/com/example/mortuie/bathlaunchpad2017/test.java
+++ app/src/main/java/com/example/mortuie/bathlaunchpad2017/test.java
@@ -3,28 +3,38 @@ package com.example.mortuie.bathlaunchpad2017;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;;
 
 // this is the API key: AIzaSyCuot73LGetaKOTpAyO0r4tZOQQAg2vu_Q
 
 
-public class test extends AppCompatActivity {
+public class test extends AppCompatActivity implements OnMapReadyCallback {
 
     int PLACE_PICKER_REQUEST = 1;
-
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-        builder.setLatLngBounds(new LatLngBounds(new LatLng(51.382378,-2.358606), new LatLng(51.387469,-2.368401)));
         try {
             startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
         } catch (GooglePlayServicesRepairableException e) {
@@ -38,15 +48,33 @@ public class test extends AppCompatActivity {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
                 Place selectedPlace = PlacePicker.getPlace(this, data);
-                Toast.makeText(this, (String)selectedPlace.getName().toString(), Toast.LENGTH_LONG).show();
-
-                // Do something with the place
+                mMap.addMarker(new MarkerOptions().position(selectedPlace.getLatLng()).title(selectedPlace.getName().toString()));
             }
         }
+    }
+
+    public void addPoint(View view){
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void submitRoute(View view){
+        //Leon writes to a file
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 
 
 
 
 
-}
+    }

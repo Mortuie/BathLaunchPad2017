9004eb22b8c2ff490c69360480dcd11ab6b6317f
ManavGupta04 <manavgupta@hotmail.co.uk>
2017-03-25 21:23:12 +0000

    Fixed leons code which took 11hrs in 11 mins

--- app/src/main/java/com/example/mortuie/bathlaunchpad2017/test.java
+++ app/src/main/java/com/example/mortuie/bathlaunchpad2017/test.java
@@ -1,51 +1,50 @@
 package com.example.mortuie.bathlaunchpad2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;;
 
 // this is the API key: AIzaSyCuot73LGetaKOTpAyO0r4tZOQQAg2vu_Q
 
 
 public class test extends AppCompatActivity {
 
+    int PLACE_PICKER_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
-        String text = "";
-        BufferedReader in = null;
-
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        builder.setLatLngBounds(new LatLngBounds(new LatLng(51.382378,-2.358606), new LatLng(51.387469,-2.368401)));
         try {
-
-            URL query = new URL("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=51.503186,-0.126446&radius=5000&type=museum&key=AIzaSyCuot73LGetaKOTpAyO0r4tZOQQAg2vu_Q");
-            //URLConnection q2 = query.openConnection();
-            in = new BufferedReader(new InputStreamReader(query.openConnection().getInputStream()));
-
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                text += inputLine + "\n";
-            }
-        } catch (MalformedURLException e) {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (GooglePlayServicesNotAvailableException e) {
             e.printStackTrace();
         }
-
-        TextView tv2 = (TextView) findViewById(R.id.textView2);
-        tv2.setText(text);
-
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place selectedPlace = PlacePicker.getPlace(this, data);
+                Toast.makeText(this, (String)selectedPlace.getName().toString(), Toast.LENGTH_LONG).show();
+
+                // Do something with the place
+            }
+        }
+    }
+
 
 
 

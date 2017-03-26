2eaabb35399fad32dd0a21a55779d5c5fd4641eb
mortuie <jimmybob-leon@hotmail.co.uk>
2017-03-26 06:07:19 +0100

    Pushed changes so it goes over Atul's server

--- app/src/main/java/com/example/mortuie/bathlaunchpad2017/GettingData.java
+++ app/src/main/java/com/example/mortuie/bathlaunchpad2017/GettingData.java
@@ -27,15 +27,13 @@ public class GettingData extends Thread{
         String lines = "";
 
         try {
-            url = new URL("https://citysherpablog.wordpress.com/about/");
+            url = new URL("http://www.kkmonlee.com/launchpad/leon.txt");
             is = url.openStream();
             br = new BufferedReader(new InputStreamReader(is));
             String line;
             while ((line = br.readLine()) != null) {
                 line = line.trim();
-                if (line.startsWith("<p>") && line.endsWith("</p>")) {
-                    line = line.replaceAll("<p>", "");
-                    line = line.replaceAll("</p>", "");
+                if (line.equals("TEST")) {
                     lines = line;
                     valueEntered = true;
                 }
